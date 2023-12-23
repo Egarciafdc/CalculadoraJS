3c2d14f31f67733dba9a8a58e9e10f8e454f72b6
Ernesto Garcia <egarciafdc@gmail.com>
2023-12-23 14:21:35 -0500

    Agregando funciones extra, como mas o menos, revisar operadores

--- index.js
+++ index.js
@@ -2,17 +2,40 @@ const displayValorAnterior = document.querySelector("#valor-anterior");
 const displayValorActual = document.querySelector("#valor-actual");
 const botonesNum = document.querySelectorAll(".numero");
 const botonesOp = document.querySelectorAll(".operador");
+const botonesExtra = document.querySelectorAll(".extra");
+const botonBorrarTodo = document.querySelector(".borrarT")
 
 const display = new Display(displayValorAnterior, displayValorActual)
+var band = 0;
 
 botonesNum.forEach(boton => {
     boton.addEventListener("click", () => {
-        display.agregarNumero(boton.innerHTML);
+        boton.classList.add(".animado");
+        if (band > 0) {
+            display.agregarNumero(boton.innerHTML);
+        }
+        else if (boton.innerHTML != 0) {
+            botonBorrarTodo.innerHTML = "C";
+            display.agregarNumero(boton.innerHTML);
+            band = 1;
+        }
     });
 });
 
 botonesOp.forEach(boton => {
     boton.addEventListener("click", () => {
+        botonBorrarTodo.innerHTML = "C";
         display.computar(boton.value);
+        band = 0;
     })
+});
+
+botonesExtra.forEach(boton => {
+    boton.addEventListener("click", () => {
+        band = 0;
+    })
+});
+
+botonBorrarTodo.addEventListener("click", () => {
+    botonBorrarTodo.innerHTML = "AC";
 })
